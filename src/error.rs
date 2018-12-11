5cc95934fb9e9a6c63db7bb100f2e2eda9b812be
Yusuke Mori <fr052123@gmail.com>
2018-12-11 12:06:57 +0900

    Add some codes (#1)
    * Add files via upload
    
    * Create DELETEME
    
    * Add files via upload
    
    * Delete DELETEME

---
+++ src/error.rs
@@ -0,0 +1,110 @@
+#[derive(Fail, Debug)]
+pub enum ErrorKind {
+    #[fail(display = "Json parse error")]
+    JsonParse,
+    #[fail(display = "Fetch failed error")]
+    FetchFailed,
+}
+
+impl From<serde_json::error::Error> for Error {
+    fn from(error: serde_json::error::Error) -> Error {
+        Error {
+            inner: error.context(ErrorKind::JsonParse),
+        }
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(error: reqwest::Error) -> Error {
+        Error {
+            inner: error.context(ErrorKind::FetchFailed),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn serde_json_error_test() {
+        #[derive(Deserialize)]
+        struct SomeType {}
+
+        fn tester() -> Result<SomeType, super::Error> {
+            serde_json::from_str("invalid string").map_err(Into::into)
+        }
+
+        match tester() {
+            Ok(_) => assert!(false),
+            Err(err) => match err.kind() {
+                super::ErrorKind::JsonParse => { /*OK*/ }
+                _ => assert!(false),
+            },
+        }
+    }
+
+    #[test]
+    fn reqwest_error_test() {
+        fn tester() -> Result<reqwest::Response, super::Error> {
+            reqwest::get("invalid url").map_err(Into::into)
+        }
+
+        match tester() {
+            Ok(_) => assert!(false),
+            Err(err) => match err.kind() {
+                super::ErrorKind::FetchFailed => { /*OK*/ }
+                _ => assert!(false),
+            },
+        }
+    }
+}
+
+// Fxxkin boilerplate from https://boats.gitlab.io/failure/error-errorkind.html
+use failure::{Backtrace, Context, Fail};
+use std::fmt;
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub struct Error {
+    inner: Context<ErrorKind>,
+}
+
+impl Fail for Error {
+    fn cause(&self) -> Option<&Fail> {
+        self.inner.cause()
+    }
+
+    fn backtrace(&self) -> Option<&Backtrace> {
+        self.inner.backtrace()
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Display::fmt(&self.inner, f)
+    }
+}
+
+impl Error {
+    pub fn new(inner: Context<ErrorKind>) -> Error {
+        Error { inner }
+    }
+
+    pub fn kind(&self) -> &ErrorKind {
+        self.inner.get_context()
+    }
+}
+
+impl From<ErrorKind> for Error {
+    fn from(kind: ErrorKind) -> Error {
+        Error {
+            inner: Context::new(kind),
+        }
+    }
+}
+
+impl From<Context<ErrorKind>> for Error {
+    fn from(inner: Context<ErrorKind>) -> Error {
+        Error { inner }
+    }
+}
+//end boilerplate
---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+#[macro_use]
+extern crate serde_derive;
+
+mod error;
+mod fetcher;
+
+pub use self::error::Error;
+pub use self::fetcher::{Jwks, JwksFetcher};
+
+pub fn verify<'a, P>(jwt: String) -> Result<P, Error>
+where
+    P: serde::Deserialize<'a>,
+{
+    serde_json::from_str("{\"iss\": \"hoge\"}").map_err(Into::into)
+}
+
+#[cfg(test)]
+mod tests {
+    // use crate::JwksFetcher;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn verify_test() {
+        #[derive(Deserialize)]
+        struct MyPayload {
+            iss: String,
+        }
+
+        match super::verify::<MyPayload>("aaa".to_owned()) {
+            Ok(payload) => println!("{}", payload.iss),
+            Err(err) => panic!("{:?}", err),
+        }
+    }
+
+    #[test]
+    fn fetch_test() {
+        // let fetcher = super::MyJwksFetcher {};
+        // match fetcher.fetch("https://payper.auth0.com/.well-known/jwks.json".to_owned()) {
+        //     Ok(value) => {
+        //         println!("{:?}", value);
+        //     }
+        //     Err(err) => {
+        //         println!("{:?}", err);
+        //     }
+        // }
+    }
+}
