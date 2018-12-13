f277f5235bc8e30de362ad1f530c897d32143376
川田 恵氏 (Kawada Keishi a.k.a megumish) <megumish@exploitouri.st>
2018-12-13 17:21:04 +0900

    Refactoring for some tests (#6)
    * try desirizlize owned.
    
    * refactoring for some test cases

--- src/decode.rs
+++ src/decode.rs
@@ -14,61 +14,37 @@ where
     signature: String,
 }
 
-/// Decode jwt to provided struct.
-///
-/// ## Example
-///
-/// ```
-/// #[macro_use]
-/// extern crate serde_derive;
-///
-/// extern crate suzume;
-///
-/// #[derive(Deserialize)]
-/// struct Header {
-///     alg: String,
-///     typ: String,
-/// }
-///
-/// #[derive(Deserialize)]
-/// struct Payload {
-///     sub: i32,
-///     name: String,
-///     iat: i32,
-/// }
-///
-/// fn main() {
-///     let jwt =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c".to_owned();
-///     let decoded = suzume::from_raw_jwt::<Header, Payload>(jwt);
-/// }
-/// ```
-pub fn from_raw_jwt<H, P>(jwt: String) -> Result<Decoded<H, P>, Error>
-where
-    H: serde::de::DeserializeOwned,
-    P: serde::de::DeserializeOwned,
-{
+fn split_jwt(jwt: &str) -> Result<Vec<&str>, Error> {
     const DELIMITER: &str = ".";
     let splitted = jwt.split(DELIMITER).collect::<Vec<&str>>();
     if splitted.len() != 3 {
         return Err(Error::from(ErrorKind::WrongToken));
+    } else {
+        Ok(splitted)
     }
-
-    let decoded = Decoded {
-        header: token_to_struct::<H>(splitted[0])?,
-        payload: token_to_struct::<P>(splitted[1])?,
-        signature: splitted[2].to_owned(),
-    };
-
-    Ok(decoded)
 }
 
-fn token_to_struct<T>(s: &str) -> Result<T, Error>
+fn decode<T>(s: &str) -> Result<T, Error>
 where
     T: serde::de::DeserializeOwned,
 {
     let decoded =
         base64::decode_config(s, base64::URL_SAFE_NO_PAD).map_err::<Error, _>(Into::into)?;
     serde_json::from_slice::<T>(&decoded).map_err::<Error, _>(Into::into)
+}
+
+pub fn from_raw_jwt<H, P>(jwt: String) -> Result<Decoded<H, P>, Error>
+where
+    H: serde::de::DeserializeOwned,
+    P: serde::de::DeserializeOwned,
+{
+    let splitted = split_jwt(&jwt)?;
+
+    Ok(Decoded {
+        header: decode(splitted[0])?,
+        payload: decode(splitted[1])?,
+        signature: splitted[2].to_owned(),
+    })
 }
 
 impl From<base64::DecodeError> for Error {
@@ -85,8 +61,34 @@ impl From<serde_json::error::Error> for Error {
 
 #[cfg(test)]
 mod tests {
+
+    #[derive(Deserialize)]
+    struct Header {
+        alg: String,
+        typ: String,
+    }
+
+    #[derive(Deserialize)]
+    struct Payload {
+        sub: i32,
+        name: String,
+        iat: i32,
+    }
+
     #[test]
-    fn decode_test() {
-        //TODO: write exception test.
+    fn split_jwt() -> Result<(), super::Error> {
+        let jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\
+                   .eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ\
+                   .SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+        let raw_header = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+        let raw_payload =
+            "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ";
+        let raw_signature = "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+        assert_eq!(
+            super::split_jwt(jwt)?,
+            vec![raw_header, raw_payload, raw_signature]
+        );
+        Ok(())
     }
 }
